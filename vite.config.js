81d61e7bc0e00c25d2fc24af23753ead7024dd30
Jacob Gladfelter <59100743+um-jglad@users.noreply.github.com>
2025-09-03 15:55:59 -0400

    Change base path in Vite configuration
    repo name changed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/um-gl-cost-calc/',
+  base: '/um-hpc-cost-calc/',
 })
